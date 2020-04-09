966308302d4768ad02806c7e3a917255b8a944c6
kat-hi <s77518@beuth-hochschule.de>
2020-04-09 22:57:17 +0200

    refactoring: change var names to be clearer about usage

--- api/sweb_backend/config.py
+++ api/sweb_backend/config.py
@@ -6,11 +6,10 @@ class Config:
 	# DATABASE
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI_DEV']
 
-
 	# SECRETS
 	GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
 	GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
-	SECRET_KEY = os.environ['SECRET_KEY']
+	SESSION_SECRET_KEY = os.environ['SECRET_KEY']
 
 	# LOGIN
 	ADMIN_EMAIL_1 = os.environ['ADMIN_EMAIL_1']
@@ -20,13 +19,14 @@ class Config:
 	ADMIN_BASE_URL = os.environ['ADMIN_BASE_URL']
 
 	# SMTP
-	PORT = os.environ['SMTP_PORT']
-	SERVER = os.environ['SMTP_SERVER']
-	SENDER = os.environ['SENDER_EMAIL']
-	RECEIVER = os.environ['RECEIVER_EMAIL']
-	PASSWORD = os.environ['SMTP_PASSWORD']
+	SMTP_PORT = os.environ['SMTP_PORT']
+	SMTP_SERVER = os.environ['SMTP_SERVER']
+	SENDER_EMAIL = os.environ['SENDER_EMAIL']
+	RECEIVER_EMAIL = os.environ['RECEIVER_EMAIL']
+	SMTP_PASSWORD = os.environ['SMTP_PASSWORD']
 
 	IMAGE_BASE_URL = os.environ['IMAGE_BASE_URL']
+
 
 class Production(Config):
 	# DATABASE
