714578c6af234ad137dc7ac39810e54716aed509
wkrl <kwxrl@yandex.com>
2021-04-08 23:32:09 +0200

    Remove 'Mehr info' button in InfoBox

--- app/src/components/TreeInfoBox.js
+++ app/src/components/TreeInfoBox.js
@@ -50,70 +50,15 @@ const useStyles = makeStyles(theme => ({
 
 const TreeInfoBox = (props) => {
     const classes = useStyles();
-    const [showInfo, setShowInfo] = useState(false);
     const [data, setData] = useState({response: null});
     const [error, setError] = useState("");
 
-    const handleClose = (event, reason) => {
+    const handleClose = (_, reason) => {
         if (reason === 'clickaway') {
           return;
         }
         setError(false);
     };
-
-    const renderMoreInfoButton = () => {
-        return (!showInfo &&
-            <Grid container>
-                <Grid item xs={12}>
-                    <Button className={classes.moreInfoButton} size="small" color="primary" onClick={() => setShowInfo(!showInfo)}>
-                        Mehr Infos
-                    </Button>
-                </Grid>
-            </Grid>)
-    }
-
-    const renderMoreInfoField = () => {
-        return (showInfo &&
-            <Grid container spacing={1}>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">Andere Namen</Typography>
-                    <Typography variant="body2">{data.response[0].andereNamen}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">Früchte</Typography>
-                    <Typography variant="body2">{data.response[0].beschreibung}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">Verbreitung</Typography>
-                    <Typography variant="body2">{data.response[0].verbreitung}</Typography>
-                </Grid>
-            </Grid>)
-    }
-
-    const renderCardActions = () => {
-        return (<CardActions>
-            <Button size="small" 
-                component={NavLink} 
-                to={{
-                    pathname: "/anfrage", 
-                    state: { 
-                        treeId: props.treeId, 
-                        treeName: data.response[0].sorte, 
-                        reihe:  props.reihe, 
-                        pflanzreihePosition: props.pflanzreihePosition
-                    }
-                }}
-                variant="contained" 
-                style={{color: 'white', backgroundColor: 'rgb(236, 108, 63)'}}
-            >
-                Pate werden
-            </Button>
-            { showInfo &&
-            <Button size="small" color="primary" onClick={() => setShowInfo(!showInfo)}>
-                Weniger Infos
-            </Button>}
-        </CardActions>)
-    }
 
     useEffect(() => {        
         axios.get("https://swebapi.demo.datexis.com/api/karte/baeume")
@@ -161,11 +106,40 @@ const TreeInfoBox = (props) => {
                             <Typography variant="body2">{data.response[0].reifezeit}</Typography>
                         </Grid>
                     </Grid>
-                    { renderMoreInfoButton() }
-                    { renderMoreInfoField() }
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
+                            <Typography variant="subtitle1">Andere Namen</Typography>
+                            <Typography variant="body2">{data.response[0].andereNamen}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="subtitle1">Früchte</Typography>
+                            <Typography variant="body2">{data.response[0].beschreibung}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="subtitle1">Verbreitung</Typography>
+                            <Typography variant="body2">{data.response[0].verbreitung}</Typography>
+                        </Grid>
+                    </Grid>
                 </div>
             </CardContent>
-            { renderCardActions() }
+            <CardActions>
+                <Button size="small" 
+                    component={NavLink} 
+                    to={{
+                        pathname: "/anfrage", 
+                        state: { 
+                            treeId: props.treeId, 
+                            treeName: data.response[0].sorte, 
+                            reihe:  props.reihe, 
+                            pflanzreihePosition: props.pflanzreihePosition
+                        }
+                    }}
+                    variant="contained" 
+                    style={{color: 'white', backgroundColor: 'rgb(236, 108, 63)'}}
+                >
+                    Pate werden
+                </Button>                
+            </CardActions>
         </Card>}
         <Snackbar open={error} autoHideDuration={6000} onClose={handleClose}>
             <SnackbarContent
